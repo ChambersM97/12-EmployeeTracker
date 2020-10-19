ecb2542aa7bc71c485915e101c17ebeffe794846
Chambers Mansour <chambersmansour97@gmail.com>
2020-10-19 00:19:50 -0500

    add more values for database in seeds.sql

--- MySQL/seeds.sql
+++ MySQL/seeds.sql
@@ -1,2 +1,15 @@
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Chambers", "Mansour", 1, 110198);
+VALUES ("Chambers", "Mannymouse", 1, 1101);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Parker", "Manderine", 2, 2409);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sophie", "Heathren", 1, 3780);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Hannah", "Gucci", 1, 5293);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Peach", "Covington", 1, 8414);
+
